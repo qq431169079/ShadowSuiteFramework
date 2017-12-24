a4540738efba0a0d9d5611bd22383a37e2711fbd
Catayao56 <Catayao56@gmail.com>
2017-12-24 22:31:18 +0800

    It's name is no longer describes its real purpose. It's no longer a framework. It's now a toolkit. Shadow Suite version 0.0.2.0! Happy new year!

--- src/ShadowFramework.c
+++ src/ShadowFramework.c
@@ -1,4 +1,4 @@
-/* Shadow Framework :: Ethical Hacking Toolkit Framework
+/* Shadow Suite :: Ethical Hacking Toolkit
  * Copyright (C) 2017  Shadow Team <Public.ShadowTeam@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
@@ -29,6 +29,15 @@ void prn_invalidinput(void); //Prints the invalid input message.
 void exit_confirm(void); //Asks for exit confirmation.
 void prn_construction(void); //Prints the under construction message.
 void prn_logo(void); //Prints our logo.
+void reconSection(void); //Reconnaissance section
+ void infoga(void);
+void scanSection(void); //Scanning section
+void crackSection(void); //Encryption and Password Cracking section
+void exploitSection(void); //Exploitation section
+void dosSection(void); //Denial-of-Service section
+void hijackSection(void); //Session Hijacking section
+void instdeps(void); //Install Dependencies function
+void prn_help(void); //Help function 
 
 void clr(void)  //Clear function
 {
@@ -37,13 +46,14 @@ void clr(void)  //Clear function
 
 void readline(void)
 {
-    system("read temporary"); //Command lets user input any word, even
+    system("read dummy"); //Command lets user input any word, even
                               //a line break.
 }
 
 void prn_invalidinput(void)
 {
     clr();
+    prn_logo();
     printf("\nERROR: Invalid Input\n");
     readline();
 }
@@ -56,6 +66,7 @@ void exit_confirm(void)
     while (error_value == 1)
     {
     clr();
+    prn_logo();
     printf("\nDo you really want to quit?\nAnswer (y/n) : \n");
     scanf(" %c", &confirm);
     if (confirm == 'y' || confirm == 'Y')
@@ -80,9 +91,8 @@ void exit_confirm(void)
 
 void prn_construction(void)
 {
-    clr();
+    prn_logo();
     printf("\nERROR: Under Construction!\n");
-    readline();
 }
 
 //prn_logo(); Function
@@ -90,40 +100,202 @@ void prn_logo(void)
 {
         //In the future, it needs to be standalone!
         //which means it doesn't need any third-party tools!
-    system("figlet -f shadow Shadow Framework");
+    system("figlet -f shadow Shadow Suite");
+    printf("\n\"Ethical Hacking Toolkit\"\n");
+}
+
+void infoga(void)
+{
+    clr();
+    system("cd $PWD/sections/reconnaissance/infoga/ && python2 infoga.py");
+    system("cd ../../..");
+}
+
+void redhawk(void)
+{
+    clr();
+    system("cd $PWD/sections/reconnaissance/redhawk/ && php rhawk.php");
+    system("cd ../../..");
+}
+
+void set(void)
+{
+    clr();
+    system("cd $PWD/sections/reconnaissance/set/ && python2 set");
+    system("cd ../../..");
+}
+
+void urlcrazy(void)
+{
+    clr();
+    system("cd $PWD/sections/reconnaissance/urlcrazy/ && ruby urlcrazy");
+    system("cd ../../..");
+}
+
+void weeman(void)
+{
+    clr();
+    system("cd $PWD/sections/reconnaissance/weeman/ && python2 weeman.py");
+    system("cd ../../..");
+}
+
+void reconSection(void)
+{
+    int reconSectionInput;
+
+    clr();
+    prn_logo();
+    printf("\n==RECONNAISSACE SECTION==\n\n---------------------------------\n\n");
+    printf("\n[1] Infoga\n[2] Red Hawk\n[3] Social Engineer Toolkit [!] ROOT REQUIRED\n[4] URLCrazy\n[5] Weeman\n\n[99] Back\n");
+    scanf("\n%d", &reconSectionInput);
+    switch (reconSectionInput)
+    {
+        case 1:
+		infoga();
+		break;
+
+	case 2:
+		redhawk();
+		break;
+
+	case 3:
+		set();
+		break;
+
+	case 4:
+		urlcrazy();
+		break;
+
+	case 5:
+		weeman();
+		break;
+
+	case 99:
+		main();
+		break;
+
+	default:
+		prn_invalidinput();
+		break;
+    }
+}
+
+void scanSection(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==SCANNING SECTION==\n\n---------------------------------\n\n");
+    prn_construction();
+    readline();
+}
+
+void crackSection(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==ENCRYPTION AND PASSWORD CRACKING SECTION==\n\n---------------------------------\n\n");
+    prn_construction();
+    readline();
+}
+
+void exploitSection(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==EXPLOITATION SECTION==\n\n---------------------------------\n\n");
+    prn_construction();
+    readline();
+}
+
+void dosSection(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==DENIAL-OF-SERVICE SECTION==\n\n---------------------------------\n\n");
+    prn_construction();
+    readline();
+}
+
+void hijackSection(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==SESSION HIJACKING SECTION==\n\n---------------------------------\n\n");
+    prn_construction();
+    readline();
+}
+
+void instdeps(void)
+{
+    int batch = 4;
+    clr();
+    prn_logo();
+    printf("\n==INSTALL DEPENDENCIES==\n\n---------------------------------\n\n");
+    printf("\nInstalling dependencies...\n");
+    system("apt update -y");
+    system("apt install apache2 apache2-dev bash bash-completion bash-dev binutils  binutils-dev bsdtar busybox command-not-found coreutils cowsay curl debianutils dialog diffutils dnsutils figlet findutils git gnupg gnupg2 inetutils iperf3 less lftp lighttpd neofetch net-tools netcat nginx nmap openssh openssl openssl-dev openssl-tool perl php php-apache php-dev php-pgsql postgresql privoxy proxychains-ng python python-dev python2 python2-dev radare2 radare2-dev readline readline-dev ruby ruby-dev sl sslscan tar tor torsocks tracepath tree util-linux vim w3m wget zip");
+    system("pip install requests urllib3 shodan");
+    printf("\nInstalling dependencies... Done!\n");
+    printf("Press any key to continue...");
+    readline();
 }
 
 //Main Function
 int main(void)
 {
-    char command[100];
+    char justarandomone = 'x';
+    int section;
 
+    while (justarandomone == 'x')
+    {
     clr();
     //Prints the logo of Shadow Framework..
     prn_logo();
-    printf("\nEthical Hacking Toolkit Framework\n\n\n");
-    printf("$: ");
-    scanf("%s", &command);
-    //Command query; If variable 'command' matcher one of here,
-    //then the statement is executed.
-    if (command == "help")
-    {
-        printf("help called.");
-    }
+    printf("\n==MODULES==\n\n---------------------------------\n\n");
+    printf("\n[1] Reconnaissance\n[2] Scanning\n[3] Encryption and Password Cracking\n");
+    printf("[4] Exploitation\n[5] Denial-of-Service\n[6] Session Hijacking\n");
+    printf("\n[98] Install Dependencies\n[99] Exit\n");
 
-    else if (command == "settings")
-    {
-        printf("settings called.");
-    }
+    scanf("\n%d", &section);
 
-    else if (command == "exit")
-    {
-        exit_confirm();
-    }
+    switch (section)
+        {
+        case 1:
+		reconSection();
+		break;
 
-    else
-    {
-        prn_invalidinput();
+	case 2:
+		scanSection();
+		break;
+
+	case 3:
+		crackSection();
+		break;
+
+	case 4:
+		exploitSection();
+		break;
+
+	case 5:
+		dosSection();
+		break;
+
+	case 6:
+		hijackSection();
+		break;
+
+	case 98:
+		instdeps();
+		break;
+
+	case 99:
+		exit_confirm();
+		break;
+
+	default:
+    		prn_invalidinput();
+    		break;
+        }
     }
-    main();
+    return 1; //Program must not go here!!!
 }
