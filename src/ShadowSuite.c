6c00a9ae62d0709cc58ce144c0cc535ebdc953e2
Catayao56 <Catayao56@gmail.com>
2017-12-26 16:54:04 +0800

    Renamed sections directory to tools. Added new tools

--- src/ShadowSuite.c
+++ src/ShadowSuite.c
@@ -15,6 +15,15 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/* Colors
+ * W  = '\033[0m'  # white (normal)
+ * R  = '\033[31m' # red
+ * G  = '\033[32m' # green
+ * O  = '\033[33m' # orange
+ * B  = '\033[1m'  # bold
+ * RR = '\033[3m'  # greencolor
+ */
+
 //Include preprocessor directives
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,18 +38,34 @@ void prn_invalidinput(void); //Prints the invalid input message.
 void exit_confirm(void); //Asks for exit confirmation.
 void prn_construction(void); //Prints the under construction message.
 void prn_logo(void); //Prints our logo.
-void reconSection(void); //Reconnaissance section
+void reconSection(void); //Reconnaissance tools
  void infoga(void);
-void scanSection(void); //Scanning section
-void crackSection(void); //Encryption and Password Cracking section
-void exploitSection(void); //Exploitation section
-void dosSection(void); //Denial-of-Service section
-void hijackSection(void); //Session Hijacking section
+ void redhawk(void);
+ void set(void);
+ void urlcrazy(void);
+ void weeman(void);
+ void dnsmap(void);
+ void automater(void);
+ void angryfuzzer(void);
+ void metagoofil_remote(void);
+ void metagoofil_local(void);
+ void theharvester(void);
+ void reconhelp(void);
+void scanSection(void); //Scanning tools
+ void dtect(void);
+ void dsss(void);
+ void ciscoauditingtool(void);
+ void scanhelp(void);
+void crackSection(void); //Encryption and Password Cracking tools
+void exploitSection(void); //Exploitation tools
+void dosSection(void); //Denial-of-Service tools
+void hijackSection(void); //Session Hijacking tools
 void instdeps(void); //Install Dependencies function
 void prn_help(void); //Help function 
 
-void clr(void)  //Clear function
+void clr(void)  //Clear and set color function
 {
+    system("echo -e \033[32m"); //Sets terminal color to green.
     system("clear"); //Actual command that clears the screen
 }
 
@@ -74,6 +99,7 @@ void exit_confirm(void)
         clr();
         error_value = 0;
         printf("\nExiting...\n");
+	system("echo -e \033[0m""");
         exit(EXIT_SUCCESS);
     }
     if (confirm == 'n' || confirm == 'N')
@@ -109,7 +135,7 @@ void infoga(void)
     clr();
     prn_logo();
     printf("\n==INFOGA (RECONNAISSANCE)==\n\n---------------------------------\n\n");
-    system("cd $PWD/sections/reconnaissance/infoga/ && bash infoga_wrapper.sh");
+    system("cd $PWD/tools/reconnaissance/infoga/ && bash infoga_wrapper.sh");
     system("cd ../../..");
 }
 
@@ -117,7 +143,7 @@ void redhawk(void)
 {
     clr();
     prn_logo();
-    system("cd $PWD/sections/reconnaissance/redhawk/ && php rhawk.php");
+    system("cd $PWD/tools/reconnaissance/redhawk/ && php rhawk.php");
     system("cd ../../..");
 }
 
@@ -125,7 +151,7 @@ void set(void)
 {
     clr();
     prn_logo();
-    system("cd $PWD/sections/reconnaissance/set/ && python2 set");
+    system("cd $PWD/tools/reconnaissance/set/ && python2 set");
     system("cd ../../..");
 }
 
@@ -133,7 +159,7 @@ void urlcrazy(void)
 {
     clr();
     prn_logo();
-    system("cd $PWD/sections/reconnaissance/urlcrazy/ && chmod +x urlcrazy-wrapper && ./urlcrazy-wrapper");
+    system("cd $PWD/tools/reconnaissance/urlcrazy/ && chmod +x urlcrazy-wrapper && ./urlcrazy-wrapper");
     system("cd ../../..");
 }
 
@@ -141,7 +167,7 @@ void weeman(void)
 {
     clr();
     prn_logo();
-    system("cd $PWD/sections/reconnaissance/weeman/ && python2 weeman.py");
+    system("cd $PWD/tools/reconnaissance/weeman/ && python2 weeman.py");
     system("cd ../../..");
 }
 
@@ -149,7 +175,7 @@ void dnsmap(void)
 {
     clr();
     prn_logo();
-    system("cd $PWD/sections/reconnaissance/dnsmap/ && python2 dnsmap.py");
+    system("cd $PWD/tools/reconnaissance/dnsmap/ && python2 dnsmap.py");
     system("cd ../../..");
     readline();
 }
@@ -158,8 +184,59 @@ void automater(void)
 {
     clr();
     prn_logo();
-    system("cd $PWD/sections/reconnaissance/automater/ && bash automater-wrapper");
+    system("cd $PWD/tools/reconnaissance/automater/ && bash automater-wrapper");
     system("cd ../../..");
+}
+
+void angryfuzzer(void)
+{
+    clr();
+    prn_logo();
+    system("cd $PWD/tools/reconnaissance/angryfuzzer/ && bash angryfuzzer-wrapper");
+    system("cd ../../..");
+}
+
+void metagoofil_remote(void)
+{
+    clr();
+    prn_logo();
+    system("cd $PWD/tools/reconnaissance/metagoofil/ && bash metagoofil-wrapper-remote");
+    system("cd ../../..");
+}
+
+void metagoofil_local(void)
+{
+    clr();
+    prn_logo();
+    system("cd $PWD/tools/reconnaissance/metagoofil/ && bash metagoofil-wrapper-local");
+    system("cd ../../..");
+}
+
+void theharvester(void)
+{
+    clr();
+    prn_logo();
+    system("cd $PWD/tools/reconnaissance/theharvester && bash theharvester-wrapper");
+    system("cd ../../..");
+}
+
+void reconhelp(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==RECONNAISSANCE TOOLS HELP==\n\n---------------------------------\n\n");
+    printf("Infoga: Email Information Gathering tool\n\n");
+    printf("Red Hawk: Website Information Gathering tool\n\n");
+    printf("Social Engineer Toolkit: Social Engineering tool\t\t[i] ROOT REQUIRED\n\n");
+    printf("URLCrazy: study domainname typos and URL hijacking\n\n");
+    printf("Weeman: HTTP Server for phishing.\n\n");
+    printf("DNSMap: discover the target company's IP netblocks, domain names, phone numbers, etc.\n\n");
+    printf("Automater: Automate the OSINT analysis of IP Addresses.\n\n");
+    printf("Angry Fuzz3r: collection of tools for pentesting to gather information and discover vulnerabilities of the targets based on Fuzzedb\n\n");
+    printf("Metagoofil: metadata extractor\n\n");
+    printf("The Harvester: tool for gathering target details.\n\n");
+    printf("\nPress enter to continue...\n");
+    readline();
 }
 
 void reconSection(void)
@@ -168,8 +245,8 @@ void reconSection(void)
 
     clr();
     prn_logo();
-    printf("\n==RECONNAISSACE SECTION==\n\n---------------------------------\n\n");
-    printf("\n[1] Infoga\n[2] Red Hawk\n[3] Social Engineer Toolkit [!] ROOT REQUIRED\n[4] URLCrazy\n[5] Weeman\n[6] DNSMap\n[7] Automater\n\n[99] Back\n");
+    printf("\n==RECONNAISSACE TOOLS==\n\n---------------------------------\n\n");
+    printf("\n[1] Infoga\n[2] Red Hawk\n[3] Social Engineer Toolkit\t\t[!] ROOT REQUIRED\n[4] URLCrazy\n[5] Weeman\n[6] DNSMap\n[7] Automater\n[8] Angry Fuzz3r\n[9] Metagoofil (Remote)\n[10] Metagoofil (Local)\n[11] The Harvester\n\n[98] Help\n[99] Back\n");
     scanf("\n%d", &reconSectionInput);
     switch (reconSectionInput)
     {
@@ -183,6 +260,7 @@ void reconSection(void)
 
 	case 3:
 		set();
+		readline(); //This is temporary while my device is not rooted.
 		break;
 
 	case 4:
@@ -201,8 +279,27 @@ void reconSection(void)
 		automater();
 		break;
 
+	case 8:
+		angryfuzzer();
+		break;
+
+	case 9:
+		metagoofil_remote();
+		break;
+
+	case 10:
+		metagoofil_local();
+		break;
+
+	case 11:
+		theharvester();
+		break;
+
+	case 98:
+		reconhelp();
+		break;
+
 	case 99:
-		main();
 		break;
 
 	default:
@@ -211,20 +308,82 @@ void reconSection(void)
     }
 }
 
-void scanSection(void)
+void dtect(void)
 {
     clr();
     prn_logo();
-    printf("\n==SCANNING SECTION==\n\n---------------------------------\n\n");
-    prn_construction();
+    system("cd $PWD/tools/scanning/dtect && python2 d-tect.py");
+    system("cd ../../..");
+}
+
+void dsss(void)
+{
+    clr();
+    prn_logo();
+    system("cd $PWD/tools/scanning/dsss && bash dsss-wrapper");
+    system("cd ../../..");
+}
+
+void ciscoauditingtool(void)
+{
+    clr();
+    prn_logo();
+    system("cd $PWD/tools/scanning/cisco-auditing-tool && bash cat-wrapper");
+    system("cd ../../..");
+}
+
+void scanhelp(void)
+{
+    clr();
+    prn_logo();
+    printf("\n==SCANNING TOOLS HELP==\n\n---------------------------------\n\n");
+    printf("D-Tect: provides multiple features and detection features which gather target information and finds different flaws in it.\n\n");
+    printf("DSSS: A fully functional SQLi Scanner\n\n");
+    printf("\nPress enter to continue...\n\n");
     readline();
+}
+
+void scanSection(void)
+{
+    int scanSectionInput;
+
+    clr();
+    prn_logo();
+    printf("\n==SCANNING TOOLS==\n\n---------------------------------\n\n");
+    printf("[1] D-Tect\n[2] DSSS\n[3] Cisco Auditing Tool\n\n[98] Help\n[99] Back\n");
+    scanf("\n%d", &scanSectionInput);
+    switch (scanSectionInput)
+    {
+	case 1:
+		dtect();
+		break;
+
+	case 2:
+		dsss();
+		break;
+
+	case 3:
+		ciscoauditingtool();
+		break;
+
+        case 98:
+		scanhelp();
+		break;
+
+	case 99:
+		break;
+
+	default:
+		prn_invalidinput();
+		break;
+    }
 }
 
 void crackSection(void)
 {
     clr();
     prn_logo();
-    printf("\n==ENCRYPTION AND PASSWORD CRACKING SECTION==\n\n---------------------------------\n\n");
+    printf("\n==ENCRYPTION AND PASSWORD CRACKING TOOLS==\n\n---------------------------------\n\n");
     prn_construction();
     readline();
 }
@@ -233,7 +392,7 @@ void exploitSection(void)
 {
     clr();
     prn_logo();
-    printf("\n==EXPLOITATION SECTION==\n\n---------------------------------\n\n");
+    printf("\n==EXPLOITATION TOOLS==\n\n---------------------------------\n\n");
     prn_construction();
     readline();
 }
@@ -242,7 +401,7 @@ void dosSection(void)
 {
     clr();
     prn_logo();
-    printf("\n==DENIAL-OF-SERVICE SECTION==\n\n---------------------------------\n\n");
+    printf("\n==DENIAL-OF-SERVICE TOOLS==\n\n---------------------------------\n\n");
     prn_construction();
     readline();
 }
@@ -251,7 +410,7 @@ void hijackSection(void)
 {
     clr();
     prn_logo();
-    printf("\n==SESSION HIJACKING SECTION==\n\n---------------------------------\n\n");
+    printf("\n==SESSION HIJACKING TOOLS==\n\n---------------------------------\n\n");
     prn_construction();
     readline();
 }
@@ -271,6 +430,13 @@ void instdeps(void)
     readline();
 }
 
+void prn_help(void)
+{
+    clr();
+    prn_logo();
+    system("less MANUAL");
+}
+
 //Main Function
 int main(void)
 {
@@ -282,10 +448,10 @@ int main(void)
     clr();
     //Prints the logo of Shadow Framework..
     prn_logo();
-    printf("\n==MODULES==\n\n---------------------------------\n\n");
+    printf("\n==TOOLS==\n\n---------------------------------\n\n");
     printf("\n[1] Reconnaissance\n[2] Scanning\n[3] Encryption and Password Cracking\n");
     printf("[4] Exploitation\n[5] Denial-of-Service\n[6] Session Hijacking\n");
-    printf("\n[98] Install Dependencies\n[99] Exit\n");
+    printf("\n[97] Help\n[98] Install Dependencies\n[99] Exit\n");
 
     scanf("\n%d", &section);
 
@@ -313,6 +479,10 @@ int main(void)
 
 	case 6:
 		hijackSection();
+		break;
+
+	case 97:
+		prn_help();
 		break;
 
 	case 98:
