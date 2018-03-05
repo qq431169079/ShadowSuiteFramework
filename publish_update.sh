9c6713cfd54a85224f9a5b7efabe0daa76170a39
Catayao56 <Catayao56@gmail.com>
2018-03-05 20:23:59 +0800

    Signed-off-by: Catayao56 <Catayao56@gmail.com>
    Renamed merge_to_master.sh to publish_update.sh as we are now using only
    one branch, which is the 'master' branch.
    
    Added Shellshocker which will be functional soon.

--- publish_update.sh
+++ publish_update.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/bash
 
-git branch
-echo "Enter your current branch:"
-read BRANCH
 echo "Enter version:"
 read VERSION
 echo ""
-echo "[i] Merging to master branch..."
+echo "[i] Now publishing update..."
 echo ""
 git status
 git gc
@@ -15,17 +12,13 @@ git add .
 git commit -as
 git status
 read TEMP
-git checkout master
-git status
-read TEMP
-git merge $BRANCH
-git status
-read TEMP
 git gc
 read TEMP
 git tag $VERSION
 git tag
+read TEMP
 git log
 read TEMP
-git checkout $BRANCH
 git status
+read TEMP
+git push
