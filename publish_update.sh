cbbb98c7104f3478ce43601db065b5588b51367f
Catayao56 <Catayao56@gmail.com>
2018-03-08 13:40:51 +0800

    Signed-off-by: Catayao56 <Catayao56@gmail.com>
    Updated README

--- publish_update.sh
+++ publish_update.sh
@@ -6,26 +6,37 @@ echo ""
 echo "[i] Now publishing update..."
 echo ""
 git status
+echo "[!] Press any key to continue..."
 read TEMP
 git clean -dfi
+echo "[!] Press any key to continue..."
 read TEMP
 git gc
+echo "[!] Press any key to continue..."
 read TEMP
 git add .
+echo "[!] Press any key to continue..."
 read TEMP
 git commit -as
+echo "[!] Press any key to continue..."
 read TEMP
 git status
+echo "[!] Press any key to continue..."
 read TEMP
 git gc
+echo "[!] Press any key to continue..."
 read TEMP
 git tag $VERSION
+echo "[!] Press any key to continue..."
 read TEMP
 git tag
+echo "[!] Press any key to continue..."
 read TEMP
 git log
+echo "[!] Press any key to continue..."
 read TEMP
 git status
+echo "[!] Press any key to continue..."
 read TEMP
 git push
 echo "[i] Update $VERSION published..."
