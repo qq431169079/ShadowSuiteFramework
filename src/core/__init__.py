09b44f5356835ced1e8fd39317da2b6ee098441b
Catayao56 <Catayao56@gmail.com>
2018-05-23 17:27:39 +0800

    Signed-off-by: Catayao56 <Catayao56@gmail.com>
    Fixed __init__.py error.

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,9 +1,15 @@
 from core import misc
 from core import version
 
-__all__ = ['ansi', 'asciigraphs', 'error', 'fullupdate', 'gethost', 'joke', 'list_module', 'logger', 'misc', 'multitasking', 'packaging', 'quote', 'requests', 'suggest', 'update', 'version']
-__name__ = "Shadow Suite Framework"
-__version__ = version.BOTH
-__author__ = 'Shadow Team'
-__copyright__ = misc.programFunctions().COPYRIGHT
-FILES = ('temp.py')
+__all__      = ['ansi', 'asciigraphs', 'error', 'fullupdate', 'gethost', 'joke', 'list_module', 'logger', 'misc', 'multitasking', 'packaging', 'quote', 'requests', 'suggest', 'update', 'version']
+
+__name__     = "Shadow Suite Framework"
+
+__version__  = "v0.1.1.5"
+__codename__ = "Uber"
+__type__     = "Experimental"
+
+__edition__  = "Master"
+
+__author__   = 'Shadow Team'
+FILES        = ('temp.py')
